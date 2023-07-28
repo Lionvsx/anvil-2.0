f60bd4747c051e5a4fd3df9b63ea43d54f595a3d
Leonard Roussard <lroussard@gmail.com>
2023-07-28 19:14:43 +0200

    fix: logger warning

--- utils/structures/BaseFunction.js
+++ utils/structures/BaseFunction.js
@@ -21,6 +21,6 @@ module.exports = class BaseFunction {
         logData ? this.consoleLogger.log(message, 'error') : this.consoleLogger.log(message, 'error', logData);
     }
     warn(message, logData = undefined) {
-        logData ? this.consoleLogger.log(message, 'warn') : this.consoleLogger.log(message, 'warning', logData);
+        logData ? this.consoleLogger.log(message, 'warn') : this.consoleLogger.log(message, 'warn', logData);
     }
 }
